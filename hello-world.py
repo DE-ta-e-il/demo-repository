4c11af6574417483ce3a6deac8fd28a48ecaa324
J-EUM <97498663+J-EUM@users.noreply.github.com>
2024-12-12 18:52:41 +0900

    [feat][TDET-14] Implement Hello World (#2)
    * feat: TDET-14 Implement Hello World
    
    * chore: TDET-14 Add exclamation mark to hello world text

---
+++ hello-world.py
@@ -0,0 +1,17 @@
+def add_numbers(a, b):
+    sum = a + b
+    return sum
+
+
+def multiply_numbers(a, b):
+    result = a * b
+    return result
+
+
+if __name__ == "__main__":
+    x = 10
+    y = 5
+    print("Sum: ", add_numbers(x, y))
+
+print("Product:", multiply_numbers(x, y))
+print("hello world!")
